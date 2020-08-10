13f0f6d7983205d9559d72b7b492219acd499aff
mcoghill <50680476+mcoghill@users.noreply.github.com>
2020-08-10 12:31:34 -0700

    More human readable

--- _functions/dsmart_custom/.blocks_per_node.R
+++ _functions/dsmart_custom/.blocks_per_node.R
@@ -1,14 +1,12 @@
-.blocks_per_node <-
-function (rows, cols, cpus, max_cells = 1000000) 
-{
-    m <- NA
-    if (max_cells < (rows * cols)) {
-        block_rows <- max_cells/cols
-        total_blocks <- rows/block_rows
-        m <- ceiling(total_blocks/cpus)
-    }
-    else {
-        m <- 2
-    }
-    return(m)
+.blocks_per_node <- function(rows, cols, cpus, max_cells = 1000000) {
+  m <- NA
+  if(max_cells < (rows * cols)) {
+    block_rows <- max_cells / cols
+    total_blocks <- rows / block_rows
+    m <- ceiling(total_blocks / cpus)
+  }
+  else {
+    m <- 2
+  }
+  return(m)
 }
